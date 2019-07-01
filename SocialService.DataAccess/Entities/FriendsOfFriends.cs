5987cc8a9fe1704e86e6bd3e09a06d74ff7fc59f
эдуард ена <djaziz12393@gmail.com>
2019-07-01 15:03:49 +0300

    add friendsOfFriends class

---
+++ SocialService.DataAccess/Entities/FriendsOfFriends.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocialService.DataAccess.Entities
+{
+    public class FriendsOfFriends
+    {
+        public int FriendId { get; set; }
+        public string UserId { get; set; }
+    }
+}
