85eedfc87765fd04a39881306a4546dbcaf67b8a
эдуард ена <djaziz12393@gmail.com>
2019-07-03 14:55:39 +0300

    add models for pagination

---
+++ SocialService.ServiceLogic/ViewModels/IndexPageView.cs
@@ -0,0 +1,13 @@
+﻿using SocialService.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocialService.ServiceLogic.ViewModels
+{
+    public class IndexPageView
+    {
+        public IEnumerable<User> Users { get; set; }
+        public PageView PageView { get; set; }
+    }
+}
---
+++ SocialService.ServiceLogic/ViewModels/PageView.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocialService.ServiceLogic.ViewModels
+{
+    public class PageView
+    {
+        private const int DEFAULT_PAGE = 1;
+
+        public PageView(int count, int pageNumber, int pageSize)
+        {
+            PageNumber = pageNumber;
+            TotalPage = (int)Math.Ceiling(count / (double)pageSize);
+        }
+        public int PageNumber { get; set; }
+        public int TotalPage { get; set; }
+        public bool HasPrevius
+        {
+            get
+            {
+                return PageNumber > DEFAULT_PAGE;
+            }
+        }
+        public bool HasNext
+        {
+            get
+            {
+                return PageNumber < TotalPage;
+            }
+        }
+
+    }
+}
